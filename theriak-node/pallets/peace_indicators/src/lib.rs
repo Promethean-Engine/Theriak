4c19d9ca0268487db8ecc0ad5443776880d58190
Andrew Plaza <andrew.plaza@parity.io>
2020-11-13 20:16:06 +0100

    Peace Indicators

--- theriak-node/pallets/peace_indicators/src/lib.rs
+++ theriak-node/pallets/peace_indicators/src/lib.rs
@@ -4,8 +4,9 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
-use frame_system::ensure_signed;
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, storage::IterableStorageMap, traits::Get};
+use frame_system::{ensure_signed, ensure_root};
+use crate::dispatch::Vec;
 
 #[cfg(test)]
 mod mock;
@@ -16,7 +17,7 @@ mod tests;
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
 	/// Because this pallet emits events, it depends on the runtime's definition of an event.
-	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+	type Event: From<Event> + Into<<Self as frame_system::Trait>::Event>;
 }
 
 // The pallet's runtime storage items.
@@ -28,17 +29,18 @@ decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
 		// Learn more about declaring storage items:
 		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-		Something get(fn something): Option<u32>;
+		PeaceIndicators get(fn indicators): map hasher(blake2_128_concat) u32 => Vec<u8>;
 	}
 }
 
 // Pallets use events to inform users when important changes are made.
 // https://substrate.dev/docs/en/knowledgebase/runtime/events
 decl_event!(
-	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
+	pub enum Event {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
-		SomethingStored(u32, AccountId),
+                IndicatorStored(u32),
+                InvestigationUnderway(u32),
 	}
 );
 
@@ -62,42 +64,32 @@ decl_module! {
 
 		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
-
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+            
+                /// Removes all current indicators and instantiates the set with new indicators
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
+		pub fn sudoSubmitNewIndicatorSet(origin, indicators: Vec<Vec<u8>>) -> dispatch::DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			Something::put(something);
-
+			ensure_root(origin)?;
+                        PeaceIndicators::drain(); 
+                         
+                        for (idx, indicator) in indicators.iter().enumerate() {
+                            // Update storage.
+			    PeaceIndicators::insert(idx as u32, indicator);
+                            Self::deposit_event(Event::IndicatorStored(idx as u32));
+                        }
+			
 			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
 			// Return a successful DispatchResult
 			Ok(())
 		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match Something::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::put(new);
-					Ok(())
-				},
-			}
-		}
+                
+                #[weight = 10_000 + T::DbWeight::get().writes(1)]
+                pub fn raiseInvestigation(origin, indicator: u32) -> dispatch::DispatchResult {
+                    ensure_root(origin)?;
+                    Self::deposit_event(Event::InvestigationUnderway(indicator));
+                    Ok(()) 
+                }
 	}
 }
