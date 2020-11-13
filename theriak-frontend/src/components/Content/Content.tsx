a7c89149e3a75921e93b6ef9d35892e400109e9f
Axel Szweda <16205289+axelio@users.noreply.github.com>
2020-11-13 16:54:48 +0100

    react project initiatlization, initial sketch (#1)

---
+++ theriak-frontend/src/components/Content/Content.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Content.css';
+
+const Content: React.FC = () => {
+    return (
+        <div className='contentContainer'>
+            <div className='epi item'>EPI COMPONENT TO BE CREATED HERE</div> 
+            <div className='chain item'>CHAIN OF TRUST COMPONENT TO BE CREATED HERE</div>
+        </div>
+    );
+
+}
+
+export default Content;
+
