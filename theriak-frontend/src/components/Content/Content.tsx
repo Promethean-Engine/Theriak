b762a05ef299b23d80210f35c093a4a162baa53a
Andrew Plaza <andrew.plaza@parity.io>
2020-11-14 21:34:45 +0100

    refactor some of trust pallet
    - CurrentlyRevoked/CurrentlyIssued now accurately reflect the size of
    the map
    - wont issue events for addresses that are already issued/revoked

--- theriak-frontend/src/components/Content/Content.tsx
+++ theriak-frontend/src/components/Content/Content.tsx
@@ -116,24 +116,8 @@ const ChainEpiList = async (): Promise<Array<Epi>> => {
 
     return epis;
 }
-/*
-const mockedUpEpiList: Array<Epi> = [
-    { id: 1, text: "Lorem ipsum dolor sit Maecenas feugiat tortor orci, eu lobortis " },
-    { id: 2, text: "Aenean convallis laoreet elit tempus pharetra. Maecenas feugiat tortor orci, eu lCurabitur vitae venenatis mauris" },
-    { id: 3, text: "Maecenas feugiat tortor orci, eu lobortis arcu elementum vitae. Maecenas convallis, leo sed p" },
-    { id: 4, text: "Aenean convallis laoreet elit tempus pharetra. Maecenas feugiat tortor orci, eu lobortis arcu elementum vitae. Maecenas convallis, leo sed p" },
-    { id: 5, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis facilisis massa fringilla accumsan. Curabitur vitae venenatis mauris, nec fermentum augue." },
-    { id: 6, text: "Lorem ipsum dolor sit Maecenas feugiat tortor orci, eu lobortis " },
-    { id: 7, text: "Aenean convallis laoreet elit tempus pharetra. Maecenas feugiat tortor orci, eu lCurabitur vitae venenatis mauris" },
-    { id: 8, text: "Maecenas feugiat tortor orci, eu lobortis arcu elementum vitae. Maecenas convallis, leo sed p" },
-    { id: 9, text: "Aenean convallis laoreet elit tempus pharetra. Maecenas feugiat tortor orci, eu lobortis arcu elementum vitae. Maecenas convallis, leo sed p" },
-    { id: 10, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis facilisis massa fringilla accumsan. Curabitur vitae venenatis mauris, nec fermentum augue." },
-    { id: 11, text: "Lorem ipsum dolor sit Maecenas feugiat tortor orci, eu lobortis " },
-    { id: 12, text: "Aenean convallis laoreet elit tempus pharetra. Maecenas feugiat tortor orci, eu lCurabitur vitae venenatis mauris" },
-    { id: 13, text: "Maecenas feugiat tortor orci, eu lobortis arcu elementum vitae. Maecenas convallis, leo sed p" },
-    { id: 14, text: "Aenean convallis laoreet elit tempus pharetra. Maecenas feugiat tortor orci, eu lobortis arcu elementum vitae. Maecenas convallis, leo sed p" },
-]
 
+/*
 const mockedUpTrustPeople: Array<TrustPerson> = [
     { id: 1, name: "Charles Adams", trustRate: 100 },
     { id: 2, name: "Yolanda Lowery", trustRate: 80 },
--- theriak-node/pallets/trust/src/lib.rs
+++ theriak-node/pallets/trust/src/lib.rs
@@ -1,6 +1,18 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get};
+//!
+//!
+//! Three Levels of Trust:
+//!
+//! - I know you and I trust you (in Trusted List)
+//! - I don't know anything about you so I don't trust you by default  (Not in revoked or trusted list)
+//! - I know you and I definitely don't trust you (in the Revoked List)
+//!
+//! 
+
+use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch,
+					traits::Get, storage::IterableStorageMap
+};
 use frame_system::ensure_signed;
 
 pub trait Trait: frame_system::Trait {
@@ -10,9 +22,9 @@ pub trait Trait: frame_system::Trait {
 decl_storage! {
 	trait Store for Module<T: Trait> as TrustModule {
 		CurrentIssued get(fn value1): u32;
-		TrustIssuance get(fn key1): map hasher(blake2_128_concat) u32 => T::AccountId;
+		TrustIssuance get(fn key1): map hasher(blake2_128_concat) T::AccountId => T::AccountId;
 		CurrentRevoked get(fn value2): u32;
-		TrustRevocation get(fn key2): map hasher(blake2_128_concat) u32 => T::AccountId;
+		TrustRevocation get(fn key2): map hasher(blake2_128_concat) T::AccountId => T::AccountId;
 	}
 }
 
@@ -23,6 +35,8 @@ decl_event!(
 	{
 		TrustIssued(AccountId, AccountId),
 		TrustRevoked(AccountId, AccountId),
+		TrustIssuanceRemoved(AccountId, AccountId),
+		TrustRevocationRemoved(AccountId, AccountId),
 	}
 );
 
@@ -39,27 +53,82 @@ decl_module! {
 
 		fn deposit_event() = default;
 
+		/// Fully give your trust to an account 
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]
 		pub fn issue_trust(origin, address: T::AccountId) -> dispatch::DispatchResult {
 			let who = ensure_signed(origin)?;
+			// WARN: THIS ITERATION IS VERY INEFFICIENT
+			// NOT SUITABLE FOR PRODUCTION
+			let mut do_insert = true;
+			for (issued, _sender) in <TrustIssuance<T>>::iter() {
+				if issued == address { do_insert = false; }
+			}
 
-			let key: u32 = CurrentIssued::get();
-			CurrentIssued::put(key + 1);
-			<TrustIssuance<T>>::insert(key, &address);
-
-			Self::deposit_event(RawEvent::TrustIssued(address, who));
+			if do_insert {
+				let key: u32 = CurrentIssued::get();
+				<TrustIssuance<T>>::insert(&address, &who);
+				CurrentIssued::put(key + 1);
+				Self::deposit_event(RawEvent::TrustIssued(address, who));
+			}
+			
 			Ok(())
 		}
 
+		/// Remove issued trust from an account, making their trust status 'Unknown'
+		#[weight = 10_000 + T::DbWeight::get().writes(1)]
+		pub fn remove_trust(origin, address: T::AccountId) -> dispatch::DispatchResult {
+			let who = ensure_signed(origin)?;
+			let mut do_remove = false;
+			for (issued, _sender) in <TrustIssuance<T>>::iter() {
+				if issued == address { do_remove = true; }
+			}
+
+			if do_remove {
+				let key = CurrentIssued::get();
+				CurrentIssued::put(key - 1);
+				<TrustIssuance<T>>::remove(&address);
+				Self::deposit_event(RawEvent::TrustIssuanceRemoved(address, who));
+			}
+
+			Ok(())
+		}
+
+		/// Revoke trust from an account
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]
 		pub fn revoke_trust(origin, address: T::AccountId) -> dispatch::DispatchResult {
 			let who = ensure_signed(origin)?;
 
-			let key: u32 = CurrentRevoked::get();
-			CurrentRevoked::put(key + 1);
-			<TrustRevocation<T>>::insert(key, &address);
+			let mut do_insert = true;
+			for (revoked, _sender) in <TrustRevocation<T>>::iter() {
+				if revoked == address { do_insert = false }
+			}
 
-			Self::deposit_event(RawEvent::TrustRevoked(address, who));
+			if do_insert {
+				let key: u32 = CurrentRevoked::get();	
+				CurrentRevoked::put(key + 1);
+				<TrustRevocation<T>>::insert(&address, &who);
+				Self::deposit_event(RawEvent::TrustRevoked(address, who));
+			}
+		
+			Ok(())
+		}
+
+		#[weight = 10_000 + T::DbWeight::get().writes(1)]
+		pub fn remove_revoked_trust(origin, address: T::AccountId) -> dispatch::DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			let mut do_remove = false;
+			for (revoked, _sender) in <TrustRevocation<T>>::iter() {
+				if revoked == address { do_remove = true }
+			}
+			
+			if do_remove {
+				let key: u32 = CurrentRevoked::get();
+				<TrustRevocation<T>>::remove(&address);
+				CurrentRevoked::put(key - 1);
+				Self::deposit_event(RawEvent::TrustRevocationRemoved(address, who));
+			}
+
 			Ok(())
 		}
 	}
