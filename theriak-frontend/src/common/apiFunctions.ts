c4bb8faa3ce6d9296cdc6525533c59d98afe9fd3
Andrew Plaza <aplaza@liquidthink.net>
2020-11-15 03:08:59 +0100

    Submitting EPIs work (#21)
    * add frontend for user
    
    * EPI list for manager frontend
    
    * submitting EPIs works
    
    * remove unused imports

--- theriak-frontend/src/common/apiFunctions.ts
+++ theriak-frontend/src/common/apiFunctions.ts
@@ -7,6 +7,7 @@ import {
     web3UseRpcProvider,
     web3FromSource
 } from '@polkadot/extension-dapp';
+// Import Keyring class and utility functions
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { Epi } from '../dataTypes';
 
@@ -39,12 +40,11 @@ async function attestNegative(id: number) {
     const allAccounts = await web3Accounts();
     let account = allAccounts[0]
     const injector = await web3FromSource(account.meta.source);
-
-
+   
     // currently we just get the first account
     // need to figure out how to get polkadot.js extension to choose accounts
     // but this is fine i guess
-
+    
     const attestExtrinsic = api.tx.peaceIndicators
         .attestNegative(id)
         .signAndSend(account.address, { signer: injector.signer }, (result) => {
@@ -86,10 +86,32 @@ async function submitEpis(epis: Array<string>) {
     const allAccounts = await web3Accounts();
     let account = allAccounts[0]
     const injector = await web3FromSource(account.meta.source);
-
-    const attestExtrinsic = api.tx.peaceIndicators
-        .sudo_submit_new_indicator_set(epis)
+   
+    const investExtrinsic = api.tx.peaceIndicators
+        .raiseInvestigation(id)
         .signAndSend(account.address, { signer: injector.signer }, (result) => {
+            console.log(`Current status is ${result.status}`);
+
+            if (result.status.isInBlock) {
+                alert(`Investigation raised and included at blockHash ${result.status.asInBlock}`);
+            } else if (result.status.isFinalized) {
+                alert(`Transaction is finalized at blockHash ${result.status.asFinalized}`)
+            }
+        })
+}
+
+async function submitEpis(epis: Array<string>) {
+    const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+    const api = await ApiPromise.create({ provider: wsProvider });
+    const allInjected = await web3Enable('Theriak Frontend');
+    const allAccounts = await web3Accounts();
+    let account = allAccounts[1]
+    const injector = await web3FromSource(account.meta.source);
+
+    const submitEpisExt = api.tx.sudo.sudo(
+        api.tx.peaceIndicators
+        .sudoSubmitNewIndicatorSet(epis)
+    ).signAndSend(account.address, { signer: injector.signer }, (result) => {
             console.log(`Current status is ${result.status}`);
 
             if (result.status.isInBlock) {
