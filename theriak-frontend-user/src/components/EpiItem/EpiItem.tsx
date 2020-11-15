5cb3a593b6c325b94d6c753c104c14c9510c14cb
Andrew Plaza <andrew.plaza@parity.io>
2020-11-15 02:10:48 +0100

    add frontend for user

---
+++ theriak-frontend-user/src/components/EpiItem/EpiItem.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { Epi } from '../../dataTypes';
+import './EpiItem.css';
+
+type EpiItemProps = {
+    epi: Epi,
+    reportEpi: Function
+}
+
+const EpiItem: React.FC<EpiItemProps> = ({ epi, reportEpi }) => {
+    return (
+        <div className='epiItem'>
+            <div className='epiText'>{epi.text}</div>
+            <div className="epiIcons">
+                <FontAwesomeIcon icon={faCheckCircle} className='icon accept' onClick={() => reportEpi(epi.id, true)} />
+                <FontAwesomeIcon icon={faTimesCircle} className='icon decline' onClick={() => reportEpi(epi.id, false)} />
+            </div>
+        </div>
+    );
+
+}
+
+export default EpiItem;
+
